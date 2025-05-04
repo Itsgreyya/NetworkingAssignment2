9385eee00db849dd8f11769cd1ee1141538f5591
itsgreyya <a1896716@adelaide.edu.au>
2025-05-05 01:52:24 +0930

    forget print message, not pass the full trace

--- sr.c
+++ sr.c
@@ -153,8 +153,9 @@ void A_timerinterrupt(void)
     printf("----A: time out,resend packets!\n");
 
 /* Only retransmit the specific packets that have timed out or at the very left.*/
-    
-  tolayer3(A,buffer[(windowfirst) % WINDOWSIZE]);
+  if (TRACE > 0)
+    printf("----A: resending packet %d\n",buffer[windowfirst].seqnum);
+  tolayer3(A,buffer[windowfirst]);
   packets_resent++;
 
   if (windowcount > 0) 
